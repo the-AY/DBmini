73ffdb1d1a97a0307abd04e1b63e1dd4a68a4968
the-AY <55029883+the-AY@users.noreply.github.com>
2022-01-17 16:08:48 +0530

    v.1

--- foodbook.php
+++ foodbook.php
@@ -7,11 +7,32 @@
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <title> Food on Rails</title>
 </head>
-?php
+
+<?php
 
 include "config.php";
 
+
+$host = "localhost";
+$user ="root";
+$pwd = "";
+$db ="dbmini";
+$connect = mysqli_connect($host,$user,$pwd,$db);
+session_start();
+if(isset($_POST['Submit'])){
+    $C_ID = $_POST[''];
+    $F_IDNAME=$_POST['Item'];
+    $QUANTITY = $_POST['quantity'];
+    $PRICE = $_POST[$P];
+    $sql="INSERT INTO `food` (`F_IDNAME`, `PRICE`, `C_ID`, `QTY`)  VALUES 
+    ('$F_IDNAME', '$PRICE', '$C_ID', '$QUANTITY')";
+    
+    $connect->query($sql);
+
+    $connect->close();
+}
 ?>
+
 <body>
   <header class="text-gray-600 body-font">
     <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
@@ -38,22 +59,22 @@ include "config.php";
           <div class="flex flex-wrap -m-4">
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
               <a class="block relative h-48 rounded overflow-hidden">
-                <img alt="BreakFast" class="object-cover object-center w-full h-full block" src="..\assets\masaladosa.jpg">
+                <img alt="BreakFast" class="object-cover object-center w-full h-full block" src="assets\masaladosa.jpg">
               </a>
               <div class="mt-4">
                 <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">BreakFast</h3>
                 <h2 class="text-gray-900 title-font text-lg font-medium">Masala Dosa</h2>
-                <p class="mt-1">₹</p>
+                <p class="mt-1">₹50</p>
               </div>
             </div>
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
               <a class="block relative h-48 rounded overflow-hidden">
-                <img alt="BreakFast" class="object-cover object-center w-full h-full block" src="..\assets\original images\idlivada.png">
+                <img alt="BreakFast" class="object-cover object-center w-full h-full block" src="assets\idlivada.jpg">
               </a>
               <div class="mt-4">
                 <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">BreakFast</h3>
                 <h2 class="text-gray-900 title-font text-lg font-medium">Idli Vada</h2>
-                <p class="mt-1">₹</p>
+                <p class="mt-1">₹55</p>
               </div>
             </div>
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
@@ -63,7 +84,7 @@ include "config.php";
               <div class="mt-4">
                 <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Lunch/Dinner</h3>
                 <h2 class="text-gray-900 title-font text-lg font-medium">South Indian</h2>
-                <p class="mt-1">₹</p>
+                <p class="mt-1">₹100</p>
               </div>
             </div>
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
@@ -73,7 +94,7 @@ include "config.php";
               <div class="mt-4">
                 <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Lunch/Dinner</h3>
                 <h2 class="text-gray-900 title-font text-lg font-medium">North Indian</h2>
-                <p class="mt-1">₹</p>
+                <p class="mt-1">₹100</p>
               </div>
             </div>
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
@@ -83,7 +104,7 @@ include "config.php";
               <div class="mt-4">
                 <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Snacks</h3>
                 <h2 class="text-gray-900 title-font text-lg font-medium">Onion Pokoda</h2>
-                <p class="mt-1">price</p>
+                <p class="mt-1">₹40</p>
               </div>
             </div>
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
@@ -93,7 +114,7 @@ include "config.php";
               <div class="mt-4">
                 <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Snacks</h3>
                 <h2 class="text-gray-900 title-font text-lg font-medium">Samosa</h2>
-                <p class="mt-1">₹</p>
+                <p class="mt-1">₹20</p>
               </div>
             </div>
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
@@ -103,7 +124,7 @@ include "config.php";
               <div class="mt-4">
                 <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Deserts</h3>
                 <h2 class="text-gray-900 title-font text-lg font-medium">Gulab Jamun</h2>
-                <p class="mt-1">₹</p>
+                <p class="mt-1">₹30</p>
               </div>
             </div>
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
@@ -113,32 +134,33 @@ include "config.php";
               <div class="mt-4">
                 <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Beverage</h3>
                 <h2 class="text-gray-900 title-font text-lg font-medium">TEA</h2>
-                <p class="mt-1">₹</p>
+                <p class="mt-1">₹15</p>
               </div>
             </div>
           </div>
         </div>
+
       </section>
       <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
       <form action="/action_page.php">
         <label for="Fid">Item:</label>
-        <select name="quantity" id="quantity">
-          <option value="None">None</option>
-          <option value="F01">Masala Dosa</option>
-          <option value="F02">Idli vada</option>
-          <option value="F03">South Indian Thali</option>
-          <option value="F04">North Indian Thali</option>
-          <option value="F05">Onion Pokoda </option>
-          <option value="F06">Samosa</option>
-          <option value="F07">Gulab Jamun</option>
-          <option value="F08">Tea</option>
+        <select name="Item" id="Item">
+          <option <?php $P="0" ?> value="None">None</option>
+          <option <?php $P="50" ?> value="F01-Masala Dosa">Masala Dosa</option>
+          <option <?php $P="55" ?> value="F02-Idli vada">Idli vada</option>
+          <option <?php $P="100" ?> value="F03-South Indian Thali">South Indian Thali</option>
+          <option <?php $P="100" ?> value="F04-North Indian Thali">North Indian Thali</option>
+          <option <?php $P="40" ?> value="F05-Onion Pokoda">Onion Pokoda </option>
+          <option <?php $P="20" ?> value="F06-Samosa">Samosa</option>
+          <option <?php $P="30" ?> value="F07-Gulab Jamun">Gulab Jamun</option>
+          <option <?php $P="15" ?> value="F08-">Tea</option>
         </select>
         <br><br>
-        <form action="/action_page.php">
+       
           <label for="Fid">Quantity:</label>
           <select name="quantity" id="quantity">
             <option value="None">0</option>
-            <option value="1">1</option>
+            <option value="1">1</option> 
             <option value="2">2</option>
             <option value="3">3</option>
             <option value="4">4</option>
@@ -150,108 +172,12 @@ include "config.php";
             <option value="10">10</option>
           </select>
           <br><br>
-          <form action="/action_page.php">
-            <label for="Fid">Item:</label>
-            <select name="quantity" id="quantity">
-              <option value="None">None</option>
-              <option value="F01">Masala Dosa</option>
-              <option value="F02">Idli vada</option>
-              <option value="F03">South Indian Thali</option>
-              <option value="F04">North Indian Thali</option>
-              <option value="F05">Onion Pokoda </option>
-              <option value="F06">Samosa</option>
-              <option value="F07">Gulab Jamun</option>
-              <option value="F08">Tea</option>
-            </select>
-            <br><br>
-            <form action="/action_page.php">
-              <label for="Fid">Quantity:</label>
-              <select name="quantity" id="quantity">
-                <option value="None">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-              <br><br>
-      
-              <form action="/action_page.php">
-                <label for="Fid">Item:</label>
-                <select name="quantity" id="quantity">
-                  <option value="None">None</option>
-                  <option value="F01">Masala Dosa</option>
-                  <option value="F02">Idli vada</option>
-                  <option value="F03">South Indian Thali</option>
-                  <option value="F04">North Indian Thali</option>
-                  <option value="F05">Onion Pokoda </option>
-                  <option value="F06">Samosa</option>
-                  <option value="F07">Gulab Jamun</option>
-                  <option value="F08">Tea</option>
-                </select>
-                <br><br>
-                <form action="/action_page.php">
-                  <label for="Fid">Quantity:</label>
-                  <select name="quantity" id="quantity">
-                    <option value="None">0</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                  </select>
-                  <br><br>
-                  <form action="/action_page.php">
-                    <label for="Fid">Item:</label>
-                    <select name="quantity" id="quantity">
-                      <option value="None">None</option>
-                      <option value="F01">Masala Dosa</option>
-                      <option value="F02">Idli vada</option>
-                      <option value="F03">South Indian Thali</option>
-                      <option value="F04">North Indian Thali</option>
-                      <option value="F05">Onion Pokoda </option>
-                      <option value="F06">Samosa</option>
-                      <option value="F07">Gulab Jamun</option>
-                      <option value="F08">Tea</option>
-                    </select>
-                    <br><br>
-                    <form action="/action_page.php">
-                      <label for="Fid">Quantity:</label>
-                      <select name="quantity" id="quantity">
-                        <option value="None">0</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                      </select>
-                      <br><br>
-                          
-      
-     
-
-     
+        
+        
         <button>
-      <input type="Submit" value="Submit"  class="inline-flex items-center bg-gray-100 border-0 py-2 px-5 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0" >
+        <input type="Submit" value="Submit" name="Submit" class="inline-flex items-center bg-gray-100 border-0 py-2 px-5 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0" >
         </button>
-        </div>
       </form>
-      </div>
-
+      
 </body>
-</html>
+</html> cfx
