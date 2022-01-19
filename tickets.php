45a3918ce147fe4bbc6b4b55848e08b146e6f2c9
rudhn2001 <nayakanirudh43@gmail.com>
2022-01-19 13:01:43 +0530

    update

--- tickets.php
+++ tickets.php
@@ -1,96 +1,26 @@
 <?php
-$insert=false;
 
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "dbmini";
+    include "config.php";
 
 
-//create connection 
-$conn=mysqli_connect($servername,$username,$password,$dbname);
+    if(isset($_POST['submit'])) {
 
-if ($conn->connect_error) {
-    die("connection failed :". $conn->connect_error);
-}
+        
+        //get the input details from ticketbooking
+        $name = $_POST['name'];
+        $c_id= $_POST['c_id'];
+        $coach_type=$_POST['coach_type'];
+        $src=$_POST['$src'];
+        $dest=$_POST['$dest'];
+        $ti_type="ticket";
+        $pname1=$_POST['pname1'];
+        $pname2=$_POST['pname2'];
+        $age1=$_POST['age1'];
+        $age2=$_POST['age2'];
+        $address1=$_POST['address1'];
+        $address2=$_POST['address2'];
 
-
-    ECHO "CONNECT SUCCESSFULLY";
-
-if(isset($_POST['submit'])) {
-
-
-
-
-//assign the coach type to its coach no
-$coach_no;
-
-switch ($coach_type) {
-
-    case "sleeper":
-        $coach_no="SL101";
-        break;
-    case "first class":
-        $coach_no= "1A101";
-        break;
-    case "AC chair car":
-        $coach_no= "CC101";
-        break;
-    case "second seater":
-        $coach_no= "2S101";
-        break;
-    case "AC two tier":
-        $coach_no= "2A101";
-        break;
-    case "second seater":
-        $coach_no= "2S101";
-        break;
-
-}
-
-//insert into sql code
-$sql = "INSERT INTO TICKET(`C_ID`, `C_NAME`, `COACH_NO`, `SRC`, `DEST`)VALUES('c_id', 'name', 'coach_no', 'src', 'dest')";
-
-if($conn->query($sql)==true) {
-    $insert=true;
-
-}
-else{
-    echo "ERROR: $sql <br> $conn->error";
-}
-
-// //insert into sql table
-// $rs = mysqli_query($conn, $sql);
-
-
-// if($rs) {
-//     echo "Record inserted successfully";
-// }
-// else {
-//     echo " unsuxessful";
-// }
-$conn->close();
-}
-else {
-    echo "error";
-}
-?>
-
-<?php
-
-include "config.php";
-
-
-if(isset($_POST['submit'])) {
-
-
-    //get the input details from ticketbooking
-    $name = $_POST['name'];
-    $c_id= $_POST['c_id'];
-    $coach_type=$_POST['coach_type'];
-    $src=$_POST['$src'];
-    $dest=$_POST['$dest'];
-    //assign the coach type to its coach no
+        //assign the coach type to its coach no
         $coach_no;
 
         switch ($coach_type) {
@@ -114,19 +44,27 @@ if(isset($_POST['submit'])) {
                 $coach_no= "2S101";
                 break;
 
+            }
+
+            //all the queries
+            //inserting into ticket table
+        $query="INSERT INTO TICKET(`NAME`,`C_ID`,`COACH_NO`,`SRC`,`DEST`) VALUES ('$name','$c_id','$coach_no','$src','$dest')";
+            
+        //inserting into ticket_holder table
+
+        $query2="INSERT INTO TICKET_HOLDER(`TIH_NAME`,`AGE`,`ADDRESS`) VALUES ('$pname1','$age1','$address1')";
+        $query3="INSERT INTO TICKET_HOLDER(`TIH_NAME`,`AGE`,`ADDRESS`) VALUES ('$pname2','$age2','$address2')";
+
+        $run=mysqli_query($conn,$query,$query2,$query3) or die("connection failed");
+
+        if($run) {
+            echo "sucessfully inserted";
+            
+            
         }
-	$query="INSERT INTO FORM(`NAME`,`EMAIL`,`AGE`,`CONTACT`) VALUES ('$name','$email','$age','$contact')";
-
-	$run=mysqli_query($conn,$query) or die("connection failed");
-
-	if($run) {
-		echo "sucessfully";
-		
-		
-	}
-	else {
-		echo "unsuccessful";
-		
-	}
-}
+        else {
+            echo "unsuccessful";
+            
+        }
+    }
 ?>
