335603eca60ebf84c759f5a98cdf48a32dcec71f
the-AY <55029883+the-AY@users.noreply.github.com>
2022-01-22 18:56:06 +0530

    login update V01

--- newac.php
+++ newac.php
@@ -18,16 +18,15 @@ $db ="dbmini";
 $conn = mysqli_connect($host,$user,$pwd,$db);
 
 if(isset($_POST['submit'])) {
-
-	$C_ID=$_POST['username'];
+  $USERNAME=$_POST['username'];
 	$C_NAME=$_POST['name'];
 	$AGE=$_POST['age'];
 	$ADDRESS=$_POST['address'];
   $GENDER=$_POST['gender'];
   $CONTACT=$_POST['contact'];
   $PASSWORD=$_POST['password'];
-	$query="INSERT INTO customer (`C_ID`, `C_NAME`, `AGE`, `ADDRESS`, `GENDER`, `CONTACT`, `PASSWORD`) VALUES ('$C_ID', '$C_NAME', '$AGE', '$ADDRESS', '$GENDER', '$CONTACT', '$PASSWORD') ";
-
+  
+	$query="INSERT INTO `customer` (`C_ID`, `C_NAME`, `AGE`, `ADDRESS`, `GENDER`, `CONTACT`, `PASSWORD`, `USERNAME`) VALUES ('', '$C_NAME', '$AGE', '$ADDRESS', '$GENDER', '$CONTACT', '$PASSWORD',$USERNAME')"; 
 	$run=mysqli_query($conn,$query) or die("connection failed");
 
 	if($run) {
