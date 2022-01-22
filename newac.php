842dded700b6d23a6db8386ed3aea0a9363a0110
the-AY <55029883+the-AY@users.noreply.github.com>
2022-01-23 01:47:18 +0530

    FOOD ORDER FINAL

--- newac.php
+++ newac.php
@@ -20,14 +20,13 @@ $conn = mysqli_connect($host,$user,$pwd,$db);
 if(isset($_POST['submit'])) {
 
 	$C_ID=$_POST['username'];
-  $USERNAME=$_POST['username'];
 	$C_NAME=$_POST['name'];
 	$AGE=$_POST['age'];
 	$ADDRESS=$_POST['address'];
   $GENDER=$_POST['gender'];
   $CONTACT=$_POST['contact'];
   $PASSWORD=$_POST['password'];
-	$query="INSERT INTO customer (`C_ID`, `C_NAME`, `AGE`, `ADDRESS`, `GENDER`, `CONTACT`, `PASSWORD`,`USERNAME` ) VALUES ('$C_ID', '$C_NAME', '$AGE', '$ADDRESS', '$GENDER', '$CONTACT', '$PASSWORD','$USERNAME') ";
+	$query="INSERT INTO customer (`C_ID`, `C_NAME`, `AGE`, `ADDRESS`, `GENDER`, `CONTACT`, `PASSWORD`) VALUES ('$C_ID', '$C_NAME', '$AGE', '$ADDRESS', '$GENDER', '$CONTACT', '$PASSWORD') ";
 
 	$run=mysqli_query($conn,$query) or die("connection failed");
 
@@ -62,7 +61,7 @@ if(isset($_POST['submit'])) {
     </div>
   </header>
   
-  <form action="newac.php" method="post">
+  <form action="newacbackup.php" method="post">
     <section class="text-gray-600 body-font">
         <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
           <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
