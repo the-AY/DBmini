2001997029070e66643a4111ff8403deef9329c7
rudhn2001 <nayakanirudh43@gmail.com>
2022-01-26 02:55:32 +0530

    ticket wokring

--- test.php
+++ test.php
@@ -93,3 +93,101 @@ if(isset($_POST['submit'])) {
         echo "error";
     }
     ?>-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+    include "config.php";
+
+    session_start();
+    $username = $_SESSION["username"];
+    
+    if(isset($_POST['submit'])) {
+
+        
+        //get the input details from ticketbooking
+        $name = $_POST['name'];
+
+   
+
+        $coach_type=$_POST['coach_type'];
+
+        $src=$_POST['src'];
+
+        $dest=$_POST['dest'];
+
+        $ti_type="TICKET";
+
+        $pname1=$_POST['pname1'];
+
+        $pname2=$_POST['pname2'];
+
+        $age1=$_POST['age1'];
+
+        $age2=$_POST['age2'];
+
+        $address1=$_POST['address1'];
+
+        $address2=$_POST['address2'];
+        $price=0;
+
+        //assign the coach type to its coach no
+        $coach_no;
+
+        switch ($coach_type) {
+
+            case 1:
+                $coach_no="SL101";
+                break;
+            case 2:
+                $coach_no= "1A101";
+                break;
+            case 3:
+                $coach_no= "CC101";
+                break;
+            case 4:
+                $coach_no= "2S101";
+                break;
+            case 5:
+                $coach_no= "2A101" ;
+                break;
+            default : echo "invalid option";
+            break;
+
+            }
--- tickets.php
+++ tickets.php
@@ -1,40 +1,29 @@
+
+
+
+
 <?php
 
-    include "config.php";
+include "config.php";
 
     session_start();
     $username = $_SESSION["username"];
-    
+
     if(isset($_POST['submit'])) {
 
-        
-        //get the input details from ticketbooking
-        $name = $_POST['name'];
-
-   
-
+        $name=$_POST['name'];
         $coach_type=$_POST['coach_type'];
-
         $src=$_POST['src'];
-
         $dest=$_POST['dest'];
-
         $ti_type="TICKET";
-
         $pname1=$_POST['pname1'];
-
         $pname2=$_POST['pname2'];
-
         $age1=$_POST['age1'];
-
         $age2=$_POST['age2'];
-
         $address1=$_POST['address1'];
-
         $address2=$_POST['address2'];
         $price=0;
 
-        //assign the coach type to its coach no
         $coach_no;
 
         switch ($coach_type) {
@@ -59,16 +48,25 @@
 
             }
 
+         //queries
+    //insert into ticket table
+    $query="INSERT INTO TICKET(`C_NAME`,`C_ID`,`TI_TYPE`,`SRC`,`DEST`,`COACH_NO`) VALUES ('$name','$username','$ti_type','$src','$dest','$coach_no')";
+    $query1="INSERT INTO TICKET_HOLDER(`TIH_NAME`,`AGE`,`ADDRESS`) VALUES ('$pname1','$age1','$address1')";
+    $query2="INSERT INTO TICKET_HOLDER(`TIH_NAME`,`AGE`,`ADDRESS`) VALUES ('$pname2','$age2','$address2')";
+
+    $run=mysqli_query($conn,$query) or die("connection failed");
+    $run1=mysqli_query($conn,$query1) or die("connection failed");
+    $run2=mysqli_query($conn,$query2) or die("connection failed");
 
 
-<?php
 
-include "config.php";
-
-    session_start();
-    $username = $_SESSION["username"];
-
-    if(isset($_POST['submit'])) {
-
-        $name=$_POST['name'];
-        $username=
+    if($run and $run1 and $run2) {
+        echo "sucessfully inserted";
+        header('Refresh: 2; URL = ticketbooking.html');
+      
+    }
+    else {
+        echo "unsuccessful";
+        
+    }
+}
