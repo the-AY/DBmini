40e27d25b9f33134024d27a061ceb88ae65fa978
rudhn2001 <nayakanirudh43@gmail.com>
2022-01-26 23:25:02 +0530

    ticket diplay

--- test.php
+++ test.php
@@ -191,3 +191,68 @@ if(isset($_POST['submit'])) {
             break;
 
             }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <!-- <?php
+            include "config.php";
+            if(isset($_POST['submit'])) {
+            $username=$_POST['username'];
+            $sql="SELECT A.*,B.* FROM TICKET A,TICKET_HOLDER B WHERE A.TI_TYPE='TRAVEL' AND C_ID='$username' AND A.PNR=B.PNR";
+            $result=$conn->query($sql);
+ 
+            if($result->num_rows>0) {
+                while ($row =$result->fetch_assoc()) {
+                    echo "<tr><td>" . $row[" PNR"] . "</td><td>" . $row["C_NAME"] . $row["TIH_NAME"] . "</td><td>" . $row["ADDRESS"] . "</td><td>" . $row["COACH_NO"] . "</td><td>" . $row["SEAT_NO"] . "</td><td>" . $row["PRICE"] . "</td><td>" . $row["SRC"] . "</td><td>" . $row["DEST"] . "</td></tr>";
+                }
+             }
+             else {
+                 echo "no results";
+             }
+             $conn->close();
+         }
+         else {
+             echo "error";
+         }
+            ?> -->
--- ticket_display.php
+++ ticket_display.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 
 <html>
     <head>
@@ -37,16 +37,85 @@
                <th>DEPARTING FROM</th>
                <th>DESTINATION</th>
            </tr>
+        
+       </table> 
+        <script src="" async defer></script>
+    </body>
+</html> -->
+
+
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+        <style>
+            table {
+                border-collapse: collapse;
+                width: 100%;
+                color: #eb4034;
+                font-family: monospace;
+                font-size: 25px;
+                text-align: left;
+            }
+            th {
+                background-color: #eb4034;
+                color: white;
+            }
+            tr:nth-child(even) {background-color :#ededed}
+        </style>
+    </head>
+    <body>
+       
+       <table>
+       <tr>
+               <th>PNR</th>
+               <th>USERNAME</th>
+               <th>PASSENGER NAME</th>
+               <th>CUSTOMER NAME</th>
+               <th>COACH</th>
+               <!-- <th>SEAT NUMBER</th> -->
+               <th>PRICE</th>
+               <th>DEPARTING FROM</th>
+               <th>DESTINATION</th>
+           </tr>
            <?php
            include "config.php";
+           session_start();
+           $username = $_SESSION["username"];
            if(isset($_POST['submit'])) {
-           $username=$_POST['username'];
-           $sql="SELECT A.*,B.* FROM TICKET A,TICKET_HOLDER B WHERE A.TI_TYPE='TRAVEL' AND C_ID='$username' AND A.PNR=B.PNR";
-           $result=$conn->query($sql);
+        //    $C_ID=$_POST['username'];
+           $sql="SELECT A.*,B.* FROM TICKET A, TICKET_HOLDER B WHERE TI_TYPE='TICKET' and C_ID='$username' ";
+           $result= mysqli_query($conn, $sql);
 
-           if($result->num_rows>0) {
-               while ($row =$result->fetch_assoc()) {
-                   echo "<tr><td>" . $row[" PNR"] . "</td><td>" . $row["C_NAME"] . $row["TIH_NAME"] . "</td><td>" . $row["ADDRESS"] . "</td><td>" . $row["COACH_NO"] . "</td><td>" . $row["SEAT_NO"] . "</td><td>" . $row["PRICE"] . "</td><td>" . $row["SRC"] . "</td><td>" . $row["DEST"] . "</td></tr>";
+           if($result) {
+               while ($row =mysqli_fetch_assoc($result)) {
+                $pnr= $row['PNR'] ;
+                $c_name=$row['C_NAME'];
+                $price=$row['PRICE'];
+                $src= $row['SRC'];
+                $dest= $row['DEST'];
+                $city=$row["ADDRESS"];
+                $coach_no=$row["COACH_NO"];
+                
+
+                   echo "<tr><td>" .$pnr. "</td>
+                   <td>" . $username. " </td>
+                   <td>" . $c_name. " </td>
+                   <td>" . $row["TIH_NAME"]. " </td>
+
+                   <td>" .$coach_no. "</td>
+                   <td>" .$price. "</td>
+                   <td>" .$src. "</td>
+                   <td>" .$dest. "</td>
+                   
+                   </tr>";
                }
             }
             else {
@@ -62,3 +131,6 @@
         <script src="" async defer></script>
     </body>
 </html>
+
+<!-- <td>" .$city. "</td>
+               <th>CITY</th> -->
