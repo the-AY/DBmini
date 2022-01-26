dfa9f47d16633c61fbf1d9bcde534c07a8c2520f
the-AY <55029883+the-AY@users.noreply.github.com>
2022-01-26 20:08:11 +0530

    Update foodorder.php

--- foodorder.php
+++ foodorder.php
@@ -27,13 +27,8 @@
 
 <?php
 
-// include "config.php";
+ include "config.php";
 
-$host = "localhost";
-$user ="root";
-$pwd = "";
-$db ="dbmini";
-$conn = mysqli_connect($host,$user,$pwd,$db);
 session_start();
 $username = $_SESSION["username"];
 
