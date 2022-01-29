b67be8e264c4dcf3653017f678fb026f5a2095e3
rudhn2001 <nayakanirudh43@gmail.com>
2022-01-30 03:44:14 +0530

    Create ticketbooking.php

---
+++ ticketbooking.php
@@ -0,0 +1,14 @@
+<?php
+include "config.php";
+include "tickets.php";
+
+    session_start();
+    $username = $_SESSION["username"];
+
+$train1=array();
+$train2=array();
+$train3=array();
+$train4=array();
+$train5=array();
+$train6=array();
+
