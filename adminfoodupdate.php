f1bd86d198400467be0ead44f78aaba53c78ea5f
the-AY <55029883+the-AY@users.noreply.github.com>
2022-01-30 19:42:11 +0530

    v

--- adminfoodupdate.php
+++ adminfoodupdate.php
@@ -30,8 +30,6 @@
 
     <?php
 include 'config.php';
-
-
 if(isset($_POST['submit'])){
   
     $status=$_POST['status'];
@@ -51,10 +49,7 @@ if(isset($_POST['submit'])){
 <form action="adminfoodupdate.php" method="post">
     <section class="text-gray-600 body-font">
         <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
-          <!-- <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
-            <h1 class="title-font font-medium text-3xl text-gray-900">Create a New account</h1>
-            <p class="leading-relaxed mt-4">A new account is needed to save your progress!!</p>
-          </div> -->
+          
           <div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
             <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Enter the Customer name</h2>
             <div class="relative mb-4">
@@ -73,12 +68,14 @@ if(isset($_POST['submit'])){
         <br><br>
         
             <button type="submit" value="Update" name="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"> Update  </button>
-         
-    
-            <p class="text-xs text-gray-500 mt-3"></p>
+             <p class="text-xs text-gray-500 mt-3"></p>
           </div>
         </div>
       </section>
       </form>
 </body>
 </html>
+<!-- <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
+            <h1 class="title-font font-medium text-3xl text-gray-900">Create a New account</h1>
+            <p class="leading-relaxed mt-4">A new account is needed to save your progress!!</p>
+          </div> -->
