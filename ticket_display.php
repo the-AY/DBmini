bbd22754939bf7a7ff0dc4257be12cedd0e016f0
rudhn2001 <nayakanirudh43@gmail.com>
2022-01-31 23:35:26 +0530

    commit

--- ticket_display.php
+++ ticket_display.php
@@ -72,6 +72,25 @@
         </style>
     </head>
     <body>
+
+    <header class="text-gray-600 body-font">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
+            <a class="mr-5 hover:text-gray-900" href="home.html">Home</a>
+            <a class="mr-5 hover:text-gray-900" href="https://www.railyatri.in/train-ticket?utm_source=ET277dweb&gclid=EAIaIQobChMI8ZHZt7yu9QIV1wkrCh2OFw96EAAYASAAEgLFRfD_BwE"> Trains </a>
+            <a class="mr-5 hover:text-gray-900" href="contactus.html">Contact us</a> </a>
+            <a class="mr-5 hover:text-gray-900" href="aboutus.html" >About us</a>
+
+          </nav>
+          <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
+          <span class="ml-3 text-xl">Railway Management System</span>
+          </a>
+          <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+            <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0" ><a href="ticket.html">Back</a>
+            </button>
+          </div>
+        </div>
+      </header>
        
        <table>
        <tr>
