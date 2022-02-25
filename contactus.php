88f2b7637b851eb7cda6c4bef809b317214fe118
the-AY <55029883+the-AY@users.noreply.github.com>
2022-02-25 22:45:53 +0530

    v.01

--- contactus.php
+++ contactus.php
@@ -1,4 +1,76 @@
 
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Contact us</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
+        <a class="mr-5 hover:text-gray-900" href="index.php">Home</a>
+        <a class="mr-5 hover:text-gray-900" href="index.php"> Trains </a>
+        <a class="mr-5 hover:text-gray-900" href="contactus.html">Contact us</a> </a>
+        <a class="mr-5 hover:text-gray-900" href="aboutus.html" >About us</a>
+      </nav>
+      <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
+      <span class="ml-3 text-xl">Railway Management System</span>
+      </a>
+      <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+        <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0" ><a href="login.php">Login</a>
+        </button>
+      </div>
+    </div>
+    <section class="text-gray-600 body-font relative">
+      <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <iframe width="100%" height="100%" class="absolute inset-0" frameborder="0" title="map" marginheight="0" marginwidth="0" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.6388857007264!2d74.92318891522574!3d12.866584820654218!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba358ff28ef6cf3%3A0xe93953598f53c53c!2sSahyadri%20College%20of%20Engineering%20%26%20Management%20(Autonomous)!5e0!3m2!1sen!2sin!4v1642142520501!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy">" style="filter: grayscale(1) contrast(1.2) opacity(0.4);"></iframe>
+          <div class="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+            <div class="lg:w-1/2 px-6">
+              <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
+              <p class="mt-1">Sahyadri college of Engineering  and Management,Sahyadri Campus,Adyar ,Managalore </p>
+            </div>
+            <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
+              <a class="text-indigo-500 leading-relaxed">sahyadri@sahyadri.edu.com</a>
+              <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
+              <p class="leading-relaxed">0824-2277222</p>
+            </div>
+          </div>
+        </div>
+        <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Contact Us</h2>
+          <p class="leading-relaxed mb-5 text-gray-600">Email us with your Query</p>
+          <div class="relative mb-4">
+
+            <form action="contactus.php">
+            <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+            <input type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+          </div>
+          <div class="relative mb-4">
+            <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+            <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+          </div>
+          <div class="relative mb-4">
+            <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+            <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+          </div>
+        <div><a href="mailto:techapp4ucustomerservice@gmail.com"> 
+          <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg" >Email us</button>
+          </a></div>
+          <p class="text-xs text-gray-500 mt-3">" "</p>
+        </div>
+      </div>
+    </section>
+  </form>
+  
+  </body>
+</html>
+
 <!--
 <!DOCTYPE html>
 <html lang="en">
@@ -51,72 +123,3 @@
 </body>
 </html>
 -->
-
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Contact us</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-  </head>
-  <body>
-    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-        <a class="mr-5 hover:text-gray-900" href="index.php">Home</a>
-        <a class="mr-5 hover:text-gray-900" href="index.php"> Trains </a>
-        <a class="mr-5 hover:text-gray-900" href="contactus.html">Contact us</a> </a>
-        <a class="mr-5 hover:text-gray-900" href="aboutus.html" >About us</a>
-      </nav>
-      <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
-      <span class="ml-3 text-xl">Railway Management System</span>
-      </a>
-      <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
-        <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0" ><a href="login.php">Login</a>
-        </button>
-      </div>
-    </div>
-    <section class="text-gray-600 body-font relative">
-      <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-        <div class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-          <iframe width="100%" height="100%" class="absolute inset-0" frameborder="0" title="map" marginheight="0" marginwidth="0" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.6388857007264!2d74.92318891522574!3d12.866584820654218!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba358ff28ef6cf3%3A0xe93953598f53c53c!2sSahyadri%20College%20of%20Engineering%20%26%20Management%20(Autonomous)!5e0!3m2!1sen!2sin!4v1642142520501!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy">" style="filter: grayscale(1) contrast(1.2) opacity(0.4);"></iframe>
-          <div class="bg-white relative flex flex-wrap py-6 rounded shadow-md">
-            <div class="lg:w-1/2 px-6">
-              <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
-              <p class="mt-1">Sahyadri college of Engineering  and Management,Sahyadri Campus,Adyar ,Managalore </p>
-            </div>
-            <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
-              <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
-              <a class="text-indigo-500 leading-relaxed">sahyadri@sahyadri.edu.com</a>
-              <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
-              <p class="leading-relaxed">0824-2277222</p>
-            </div>
-          </div>
-        </div>
-        <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-          <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Contact Us</h2>
-          <p class="leading-relaxed mb-5 text-gray-600">Email us with your Query</p>
-          <div class="relative mb-4">
-
-            <form action="/action_page.php">
-            <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-            <input type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-          </div>
-          <div class="relative mb-4">
-            <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-            <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-          </div>
-          <div class="relative mb-4">
-            <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-            <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-          </div>
-          <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg" >Email us</a></button>
-          <p class="text-xs text-gray-500 mt-3">" "</p>
-        </div>
-      </div>
-    </section>
-  </form>
-    <script src="" async defer></script>
-  </body>
-</html>
--- newac.php
+++ newac.php
@@ -47,9 +47,9 @@ $C_ID=$_POST['username'];
   <header class="text-gray-600 body-font">
     <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-        <a class="mr-5 hover:text-gray-900" href="home.html"><i class="fas fa-home"></i> Home</a>
-        <a class="mr-5 hover:text-gray-900" href="https://www.railyatri.in/train-ticket?utm_source=ET277dweb&gclid=EAIaIQobChMI8ZHZt7yu9QIV1wkrCh2OFw96EAAYASAAEgLFRfD_BwE"><i class="fas fa-train"></i> Trains </a>
-        <a class="mr-5 hover:text-gray-900" href="contactus.html"><i class="fas fa-address-book"></i> Contact us</a> </a>
+        <a class="mr-5 hover:text-gray-900" href="index.php"><i class="fas fa-home"></i> Home</a>
+        <a class="mr-5 hover:text-gray-900" href="index.php"><i class="fas fa-train"></i> Trains </a>
+        <a class="mr-5 hover:text-gray-900" href="contactus.html"><i class="fas fa-address-book"></i> Contact us</a> 
         <a class="mr-5 hover:text-gray-900" href="aboutus.html" ><i class="fas fa-address-card"></i> About us</a>
       </nav>
       <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
